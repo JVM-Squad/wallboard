b1a687b5b7b1014509476733fcbd9bdbc4f532eb
Alban Auzeill <alban.auzeill@sonarsource.com>
2023-08-21 15:12:58 +0200

    Add hello

--- src/main/java/org/sonar/jvm/squad/wallboard/WallboardApplication.java
+++ src/main/java/org/sonar/jvm/squad/wallboard/WallboardApplication.java
@@ -2,12 +2,23 @@ package org.sonar.jvm.squad.wallboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
+@Controller
 public class WallboardApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(WallboardApplication.class, args);
   }
 
+  @GetMapping("/hello")
+  public String hello(@RequestParam(value = "myName", defaultValue = "Dorian") String name, Model model) {
+    model.addAttribute("name", name);
+    return "hello";
+  }
 }
+
