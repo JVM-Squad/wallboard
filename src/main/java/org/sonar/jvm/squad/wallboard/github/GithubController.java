6752fa6db0f283c5f0a8459f9eb46342ed0c000f
erwan-serandour-sonarsource <erwan.serandour@sonarsource.com>
2023-08-22 14:24:51 +0200

    fix merge conflict

--- src/main/java/org/sonar/jvm/squad/wallboard/github/GithubController.java
+++ src/main/java/org/sonar/jvm/squad/wallboard/github/GithubController.java
@@ -1,4 +1,27 @@
 package org.sonar.jvm.squad.wallboard.github;
 
+import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class GithubController {
+
+  private final GithubService githubService;
+
+  public GithubController(GithubService githubService){
+    this.githubService = githubService;
+  }
+
+  @GetMapping("/github")
+  public String mend(Model model) {
+    List<GithubService.Release> releases = githubRepos().stream().map(githubService::getRelease).toList();
+    model.addAttribute("sonarJavaReleases", releases);
+    return "github";
+  }
+
+  private static List<String> githubRepos(){
+    return List.of("sonar-java", "sonar-kotlin", "sonar-xml", "sonar-jacoco", "sonar-scanner-maven", "sonar-scanner-gradle");
+  }
 }
--- src/main/java/org/sonar/jvm/squad/wallboard/github/GithubService.java
+++ src/main/java/org/sonar/jvm/squad/wallboard/github/GithubService.java
@@ -1,4 +1,28 @@
 package org.sonar.jvm.squad.wallboard.github;
 
-public class GithubConfig {
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class GithubService {
+
+  public final RestTemplate rest;
+
+  public GithubService(RestTemplate rest){
+    this.rest = rest;
+  }
+
+  @JsonIgnoreProperties(ignoreUnknown = true)
+  public record Release(String repoName, String name, String published_at) {
+  }
+
+  public Release getRelease(String repo){
+    Release release = rest.getForObject(
+      "https://api.github.com/repos/sonarsource/"+repo+"/releases/latest",
+      Release.class
+    );
+    return new Release(repo, release.name, release.published_at);
+  }
 }
