ad35d7437b19cf8cb1ee4929ad2c4bb623c94458
erwan-serandour-sonarsource <erwan.serandour@sonarsource.com>
2023-08-23 10:23:24 +0200

    add github release information to the wallboard

--- src/main/java/org/sonar/jvm/squad/wallboard/WallboardApplication.java
+++ src/main/java/org/sonar/jvm/squad/wallboard/WallboardApplication.java
@@ -18,7 +18,7 @@ public class WallboardApplication {
   @GetMapping({"/"})
   public String index(Model model) {
     model.addAttribute("widgetRowsAndColsUrls", List.of(
-      List.of("/mend", "/todo"),
+      List.of("/mend", "/github"),
       List.of("/sonarcloud", "/todo")
     ));
     return "index";
